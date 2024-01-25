b443d0734cbdc509d087a011a08ea16b68751a65
Hatzilu <itamar246@gmail.com>
2024-01-25 17:53:41 +0200

    add and configure devDependencies

--- src/app/components/LoginForm/LoginForm.tsx
+++ src/app/components/LoginForm/LoginForm.tsx
@@ -1,13 +1,12 @@
-import React from 'react'
+import React from 'react';
 
 const LoginForm = () => {
-  return (
-    <form>
-            <label htmlFor="userName">Username</label>
-        <input type="text" alt="username" />
-    
-    </form>
-  )
-}
+	return (
+		<form>
+			<label htmlFor="userName">Username</label>
+			<input type="text" alt="username" />
+		</form>
+	);
+};
 
-export default LoginForm
+export default LoginForm;
